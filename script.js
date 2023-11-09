7660d4cf891b5b6ae21b0bd6c7e9d30157ffcbc5
ishigami-def <deffrianprayoga@students.unnes.ac.id>
2023-11-10 00:19:12 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('portfolio').addEventListener('click', function() {
+    // Mengarahkan pengguna ke halaman lain saat tombol diklik
+    window.location.href = 'index2.html';
+});
